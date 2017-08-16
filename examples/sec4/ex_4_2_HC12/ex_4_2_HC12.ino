21a2912aa86ef3746e6d18719953baee8e21d2fe
canesche <michael.canesche@gmail.com>
2017-08-16 13:15:58 -0300

    atualização

---
+++ examples/sec4/ex_4_2_HC12/ex_4_2_HC12.ino
@@ -0,0 +1,30 @@
+/**
+ * Exemplo básico para HC-12
+ */
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); //RX, TX
+
+void setup() {
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+  mySerial.begin(9600);
+}
+
+void loop() {
+  
+  if(Serial.available() > 0){ // ler a partir do serial monitor envia para o HC-12
+    String input = Serial.readString();
+    mySerial.println(input);
+    Serial.print("Voce enviou: ");
+    Serial.println(input);    
+  }
+ 
+  if(mySerial.available() > 0){ // ler a partir do HC-12 and envia para o serial monitor
+    
+    String input = mySerial.readString();
+    Serial.println(input);    
+  }
+  delay(20);
+}
